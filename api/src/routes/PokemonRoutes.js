1b3a0ede1033cc2ad35c12023772572e5a417912
Federico Siri <federicosiri9@gmail.com>
2022-04-14 17:12:34 -0300

    PI EN PROCESO

--- api/src/routes/PokemonRoutes.js
+++ api/src/routes/PokemonRoutes.js
@@ -2,7 +2,7 @@ const { default: axios } = require('axios');
 const { Router } = require('express');
 // Importar todos los routers;
 // const { conn } = require('./db.js');
-const { Pokemon } = require('../db.js');
+const { Pokemon, Type } = require('../db.js');
 // Ejemplo: const authRouter = require('./auth.js');
 const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 
@@ -12,6 +12,48 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.post('/load', async (req, res, next) =>{
+    try{
+        let ids = []
+        let pokemons = await Pokemon.findAll()
+        if(pokemons.length < 40){
+            while(ids.length < (40 - pokemons.length)){
+                let numerosRandom = parseInt(Math.random()*500)
+                if(!ids.includes(numerosRandom) && numerosRandom !== 0){
+                    ids.push(numerosRandom)
+                }
+            }    
+            const promises = ids.map(id => {
+                return new Promise( async (resolve, reject) => {
+                    let pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/'+ id)                        
+                    const pokemonIdeal = {
+                        id: uuidv4(),
+                        nombre: pokemon.data.name,
+                        vida: pokemon.data.stats[0].base_stat,
+                        fuerza: pokemon.data.stats[1].base_stat,
+                        defensa: pokemon.data.stats[2].base_stat,
+                        velocidad: pokemon.data.stats[5].base_stat,
+                        altura: pokemon.data.height,
+                        peso: pokemon.data.weight,
+                        imagen: pokemon.data.sprites.front_default
+                    }
+                    resolve(
+                        await Pokemon.create(pokemonIdeal)
+                    )
+                    reject(err => next(err))
+                })
+            });
+            await Promise.all(promises)
+        }
+        let pokeDB = await Pokemon.findAll()               
+        res.send(pokeDB)                 
+    } catch (error){
+        next(error)
+    }
+})
+
+
+
 router.get('/', async (req, res, next) =>{
     const { name } = req.query
     if(name){
@@ -20,13 +62,18 @@ router.get('/', async (req, res, next) =>{
             pokemon = await Pokemon.findAll({                
                 where: {
                     nombre: name  
-                }              
+                }, 
+                include: [
+                    {
+                        model: Type,  
+                        attributes: ['id', 'nombre']
+                    }
+                ]         
             })
             if(pokemon.length){
                 res.send(pokemon[0])
             } else {
                 pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/' + name)
-                console.log(pokemon)
                 const pokemonNombre = {
                     id: pokemon.data.id,
                     nombre: pokemon.data.name,
@@ -36,7 +83,13 @@ router.get('/', async (req, res, next) =>{
                     velocidad: pokemon.data.stats[5].base_stat,
                     altura: pokemon.data.height,
                     peso: pokemon.data.weight,
-                    imagen: pokemon.data.sprites.front_default
+                    imagen: pokemon.data.sprites.front_default,
+                    types: pokemon.data.types.map(tipo =>{
+                        return {
+                            nombre: tipo.type.name,
+                            id: tipo.slot
+                        }
+                    })
                 }    
                 res.send(pokemonNombre)
             }
@@ -49,43 +102,22 @@ router.get('/', async (req, res, next) =>{
         } 
     } else {
         try{
-            let ids = []
-            while(ids.length <= 2){
-                var numerosRandom = parseInt(Math.random()*100)
-                if(!ids.includes(numerosRandom) && numerosRandom !== 0){
-                    ids.push(numerosRandom)
-                }
-            }    
-            let pokemons = []
-            const promises = ids.map(id => {
-                return new Promise( async (resolve, reject) => {
-                    let pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/'+ id)                        
-                    const pokemonIdeal = {
-                        id: pokemon.data.id,
-                        nombre: pokemon.data.name,
-                        vida: pokemon.data.stats[0].base_stat,
-                        fuerza: pokemon.data.stats[1].base_stat,
-                        defensa: pokemon.data.stats[2].base_stat,
-                        velocidad: pokemon.data.stats[5].base_stat,
-                        altura: pokemon.data.height,
-                        peso: pokemon.data.weight,
-                        imagen: pokemon.data.sprites.front_default
+            let pokemons = await Pokemon.findAll({
+                include: [
+                    {
+                        model: Type,  
+                        attributes: ['id', 'nombre']
                     }
-                    resolve(
-                        pokemons.push(pokemonIdeal)
-                    )
-                    reject(err => next(err))
-                })
-            });
-            await Promise.all(promises)            
-            let pokeDB = await Pokemon.findAll()            
-            let resultado = [...pokemons, ...pokeDB]
-            res.send(resultado)                    
+                ]         
+            })
+            res.send(pokemons)            
         } catch(error){
             next(error)
         }
     }       
 })
+
+
 
 router.get('/:id', async (req, res, next) => { 
     const { id } = req.params
@@ -132,7 +164,7 @@ router.get('/:id', async (req, res, next) => {
 
 
 router.post('/', async (req, res, next) =>{
-    const { nombre, vida, fuerza, defensa, velocidad, altura, peso, imagen } = req.body
+    const { nombre, vida, fuerza, defensa, velocidad, altura, peso, imagen, tipos } = req.body
     try{
         const newPokemon = await Pokemon.create({
             id: uuidv4(),
@@ -145,6 +177,21 @@ router.post('/', async (req, res, next) =>{
             peso,
             imagen
         })
+
+        const promises = tipos?.map(tipo =>{
+            return new Promise (async (resolve, reject) =>{
+                let tipoBuscado = await Type.findOne({
+                    where: {
+                        nombre: tipo
+                    }
+                })
+                resolve (
+                    newPokemon.addType(tipoBuscado)
+                )
+                reject(err => next(err))
+            })
+        })       
+        await Promise.all(promises)
         res.send(newPokemon)
     } catch (error){
         next(error)
